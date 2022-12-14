7f62e73d3bac525a70a175e8204c07dcdd9d5309
Facundo-Romani <facundoromani90@gmail.com>
2022-12-14 00:14:44 -0300

    Primeros Modulos

---
+++ CursoLINQ/Modulo1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Ejecución Diferida.
+
+//int[] numeros = { 1, 2, 3, 4 };
+
+int[] numeros2 = Enumerable.Range(1, 5).ToArray();
+
+// Sitaxis 1 de methods.
+var numerosPares = numeros2.Where(n => { Console.WriteLine($"Evaluando si {n} es par"); return n % 2 == 0; }).ToList();
+
+var numerosPares2 = numeros2.Where(n => { Console.WriteLine($"Evaluando si {n} es par"); return n % 2 == 0; });
+
+// Con .ToList();
+foreach (var numero in numerosPares)
+{
+    Console.WriteLine($" si {numero} es par");
+}
+
+// Sin .ToList();
+foreach (var numero in numerosPares2)
+{
+    Console.WriteLine($" si {numero} es par");
+}
+
+// Sitaxis 2 de querys.
+//var numerosPares2 = (from n in numeros
+//                     where n % 2 == 0
+//                     select n).ToList();
---
+++ CursoLINQ/Modulo2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Función Where.
+
+int[] numeros = Enumerable.Range(1, 20).ToArray();
+
+//var numerosImpares = numeros.Where(n => n % 2 == 1).ToList();
+
+//Console.WriteLine("Los Numeros Impares son:");
+
+//foreach (var numero in numerosImpares)
+//{
+//    Console.WriteLine($"{numero}");
+//}
+
+var numerosImparesMayorQue10 = numeros.Where(n => n %2 == 1 && n > 10).ToList();
+
+Console.WriteLine("Los Numeros Impares mayor a 10 son:");
+
+foreach (var numero in numerosImparesMayorQue10) 
+{
+    Console.WriteLine($"{numero}");
+}
+
+
