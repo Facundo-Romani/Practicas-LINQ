15b6c232c359a218556bd6bcce1675d718bab495
Facundo-Romani <facundoromani90@gmail.com>
2022-12-17 00:19:33 -0300

    Modulos 1 ,2 ,3 ,4 , 5 ,6 , 7 ,8 ,9

--- CursoLINQ/Modulo8/Program.cs
+++ CursoLINQ/Modulo8/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//=====================================================================================================//
+
+// OrderBy.
+
+using Modulo3;
+
+var personas = new List<Persona>() 
+{
+            new Persona { Nombre = "Eduardo",Edad = 19, Soltero = true },
+            new Persona { Nombre = "Nidia", Edad = 25, Soltero = true },
+            new Persona { Nombre = "Alejandro", Edad = 30, Soltero = true },
+            new Persona { Nombre = "Valentina", Edad = 17, Soltero = false },
+            new Persona { Nombre = "Roberto", Edad = 18, Soltero = true },
+            new Persona { Nombre = "Eugenia", Edad = 27, Soltero = false },
+            new Persona { Nombre = "Esmerlin", Edad = 45, Soltero = false }
+};
+
+
+//var agruparPorSoltero = personas.GroupBy(p => p.Soltero);
+
+
+//foreach (var item in agruparPorSoltero)
+//{
+//    Console.WriteLine($"Grupo de personas solteros {item.Key} total {item.Count()}");
+
+//    foreach (var persona in item)
+//    {
+//        Console.WriteLine($"{persona.Nombre}");
+//    }
+//}
+
+
+var agrupamientoPorRangoEdad = personas.GroupBy(p => p.Edad / 5);
+
+
+foreach (var item in agrupamientoPorRangoEdad)
+{
+    Console.WriteLine($"Grupo de las personas en el rango de edad {item.Key * 5} - {item.Key * 5 + 5 - 1}");
+
+    foreach (var persona in item)
+    {
+        Console.WriteLine($"- {persona.Nombre} (edad: {persona.Edad})");
+    }
+}
+
---
+++ CursoLINQ/Modulo9/Program.cs
@@ -0,0 +1,57 @@
+﻿//=====================================================================================================//
+
+// InnerJoin.
+
+using Modulo4;
+using Modulo9;
+
+var personas = new List<Persona>()
+{
+                            new Persona { Nombre = "Eduardo", EmpresaId = 1, },
+                            new Persona { Nombre = "Nidia",  EmpresaId = 1 },
+                            new Persona { Nombre = "Alejandro", EmpresaId = 3 },
+                            new Persona { Nombre = "Valentina",  EmpresaId = 2 },
+                            new Persona { Nombre = "Roberto",  EmpresaId = 3 },
+                            new Persona { Nombre = "Eugenia"},
+                            new Persona { Nombre = "Esmerlin", EmpresaId = 3 }
+};
+
+
+var empresas = new List<Empresa>()
+{
+                new Empresa{Id = 1, Nombre = "Electrodomésticos Felipe"},
+                new Empresa{Id = 2, Nombre = "Bicicletas Valentina"},
+                new Empresa{Id = 3, Nombre = "Gimnasio Esmerlin"},
+                new Empresa{Id = 4, Nombre = "Ferreteria Lorenzo"}
+};
+
+
+//var personaConEmpresa = personas.Join(empresas, p => p.EmpresaId, e => e.Id, (personas, empresas) => new
+//{
+//    Persona = personas,
+//    Empresa = empresas
+//});
+
+//foreach (var i in personaConEmpresa)
+//{
+//    Console.WriteLine($"{i.Persona.Nombre} pertenece a {i.Empresa.Nombre}");
+//}
+
+//=====================================================================================================//
+
+// GroupJoin.
+
+var empresasYSusEmpleados = empresas.GroupJoin(personas, e => e.Id, p => p.EmpresaId,
+                (empresa, personas) => new { Empresa = empresa, Personas = personas });
+
+
+foreach (var item in empresasYSusEmpleados)
+{
+    Console.WriteLine($"Las siguientes personas trabajan en {item.Empresa.Nombre}");
+
+    foreach (var persona in item.Personas)
+    {
+        Console.WriteLine($"-{persona.Nombre}");
+    }
+}
+
